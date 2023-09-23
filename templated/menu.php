28f3c64d2270ca5a4aa6724cdf8474f3cd147452
Amp-Apirak <apirak.ba@gmail.com>
2023-09-23 23:23:48 +0700

    N

--- templated/menu.php
+++ templated/menu.php
@@ -28,7 +28,7 @@
                     <!-- Sidebar user panel (optional) -->
                     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                         <div class="image">
-                            <img src="../ino/img/002.png" class="img-circle elevation-2" alt="User Image">
+                            <img src="../up/img/002.png" class="img-circle elevation-2" alt="User Image">
                         </div>
                         <div class="info">
                             <a href="profile.php" class="">Administrator</a><br>
